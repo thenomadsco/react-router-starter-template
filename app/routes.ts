2c56ddf7009e15f7c5b2abb8797f0e9362787df8
thenomadsco <thenomadsco@gmail.com>
2026-02-10 15:01:04 +0530

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,6 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),
+  route("contactus", "routes/contactus.tsx"),
+] satisfies RouteConfig;

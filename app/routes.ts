32a2cf4132698d209e9c8e6a1a092a1c774e64ed
thenomadsco <thenomadsco@gmail.com>
2026-02-10 16:23:39 +0530

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("contactus", "routes/contactus.tsx"),
+  route("london", "routes/london.tsx"), // <--- Added this line
 ] satisfies RouteConfig;
